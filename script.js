3322d16370fe9ed72941efbd67611c9826826e25
ALLAHVERDIrttetw <allahverdi20242024@gmail.com>
2024-12-24 01:46:57 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Sayt tam yükləndikdən sonra çalışacaq
+    console.log("Kraker - Texnologiya və İdeyalar platformasına xoş gəlmisiniz!");
+
+    // Əgər form ilə bağlı başqa funksiyalar istəsəniz, burada əlavə edə bilərsiniz
+});
